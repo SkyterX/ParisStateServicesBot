750de75c19722979c867f262d763e51b804f2447
msamoylenko <ghost1212@mail.ru>
2017-07-24 01:07:50 +0500

    Simple polling bot using chromedriver.

--- ParisStateServicesBot/Program.cs
+++ ParisStateServicesBot/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ParisStateServicesBot
 {
@@ -10,6 +9,21 @@ namespace ParisStateServicesBot
     {
         static void Main(string[] args)
         {
+            using (var webDriver = new ChromeDriver())
+            {
+                webDriver.Navigate().GoToUrl("http://www.hauts-de-seine.gouv.fr/booking/create/9489/0");
+                webDriver.FindElement(By.XPath("//div[@id='cookies-banner']/div/a[.='Accepter']"))?.Click();
+                Thread.Sleep(1000);
+                webDriver.FindElement(By.Id("condition")).Click();
+                webDriver.FindElement(By.Name("nextButton")).Click();
+
+                webDriver.FindElement(By.Id("planning9491")).Click();
+                webDriver.FindElement(By.Name("nextButton")).Click();
+
+                var bookingText = webDriver.FindElement(By.Id("inner_Booking")).Text;
+                Console.Out.WriteLine(bookingText);
+            }
+            Console.ReadKey();
         }
     }
 }
