e6b0a0adbf944ebada8c4ce23d26cb74f60b9f2a
msamoylenko <ghost1212@mail.ru>
2017-07-24 10:09:32 +0500

    Extracted class for loading booking status + proper wait for cookies banner to disappear.

---
+++ ParisStateServicesBot/BookingStatusLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ParisStateServicesBot
+{
+    public class BookingStatusLoader : IDisposable
+    {
+        private readonly IWebDriver webDriver;
+
+        public BookingStatusLoader(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public string GetBookingStatus()
+        {
+            webDriver.Navigate().GoToUrl("http://www.hauts-de-seine.gouv.fr/booking/create/9489/0");
+
+            var cookiesBannerXPath = "//div[@id='cookies-banner']/div/a[.='Accepter']";
+            var cookiesBanner = webDriver.FindElement(By.XPath(cookiesBannerXPath));
+            cookiesBanner?.Click();
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(5))
+                .Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath(cookiesBannerXPath)));
+
+            webDriver.FindElement(By.Id("condition")).Click();
+            webDriver.FindElement(By.Name("nextButton")).Click();
+
+            webDriver.FindElement(By.Id("planning9491")).Click();
+            webDriver.FindElement(By.Name("nextButton")).Click();
+
+            return webDriver.FindElement(By.Id("inner_Booking")).Text;
+        }
+
+        public void Dispose()
+        {
+            webDriver?.Dispose();
+        }
+    }
+}
--- ParisStateServicesBot/Program.cs
+++ ParisStateServicesBot/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace ParisStateServicesBot
 {
@@ -9,20 +9,8 @@ namespace ParisStateServicesBot
     {
         static void Main(string[] args)
         {
-            using (var webDriver = new ChromeDriver())
-            {
-                webDriver.Navigate().GoToUrl("http://www.hauts-de-seine.gouv.fr/booking/create/9489/0");
-                webDriver.FindElement(By.XPath("//div[@id='cookies-banner']/div/a[.='Accepter']"))?.Click();
-                Thread.Sleep(1000);
-                webDriver.FindElement(By.Id("condition")).Click();
-                webDriver.FindElement(By.Name("nextButton")).Click();
-
-                webDriver.FindElement(By.Id("planning9491")).Click();
-                webDriver.FindElement(By.Name("nextButton")).Click();
-
-                var bookingText = webDriver.FindElement(By.Id("inner_Booking")).Text;
-                Console.Out.WriteLine(bookingText);
-            }
+            using (var loader = new BookingStatusLoader(new ChromeDriver()))
+                Console.Out.WriteLine(loader.GetBookingStatus());
             Console.ReadKey();
         }
     }
