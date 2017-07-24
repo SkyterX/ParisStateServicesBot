f3840d965583e81305ca5c8643a9bf10efec94ff
msamoylenko <ghost1212@mail.ru>
2017-07-24 12:06:11 +0500

    Periodic notification

--- ParisStateServicesBot/Program.cs
+++ ParisStateServicesBot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
+using ParisStateServicesBot.TelegramNotifications;
 
 namespace ParisStateServicesBot
 {
@@ -15,9 +18,25 @@ namespace ParisStateServicesBot
             using (var factory = new TheFactory())
             using (var bot = await factory.NotificationBot)
             {
-                var bookingStatus = factory.BookingStatusLoader.GetBookingStatus();
-                await bot.NotifyAsync(bookingStatus);
+                var token = new CancellationTokenSource();
+                Task.Run(() => RunNotifier(factory, bot, token.Token), token.Token);
                 Console.ReadKey();
+                token.Cancel();
+            }
+        }
+
+        private static async Task RunNotifier(TheFactory factory, TelegramNotificationBot bot, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var stopwatch = Stopwatch.StartNew();
+                var bookingStatus = factory.BookingStatusLoader.GetBookingStatus();
+
+                if(!bookingStatus.Contains("Vérification de disponibilité"))
+                    await bot.NotifyAsync(bookingStatus);
+
+                if (stopwatch.Elapsed.TotalMinutes < 5)
+                    await Task.Delay(TimeSpan.FromMinutes(5) - stopwatch.Elapsed, token);
             }
         }
     }
